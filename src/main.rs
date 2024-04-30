27630edf2cf5f4c10985ba4393909c453acca895
Steven Allen <steven@stebalien.com>
2024-04-30 12:29:28 -0700

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,41 @@
 use std::ffi::OsStr;
 use std::io;
+use std::str::FromStr;
 
 use ddc_hi::{Ddc, Display};
-use futures::StreamExt;
-use tokio_udev::{AsyncMonitorSocket, MonitorBuilder};
+use futures::{StreamExt, TryStreamExt};
+use tokio_udev::{AsyncMonitorSocket, Event, MonitorBuilder};
+
+fn get_attribute<T: FromStr>(event: &Event, attr: &str) -> Option<T> {
+    event
+        .attribute_value(attr)
+        .and_then(OsStr::to_str)
+        .and_then(|s| s.parse().ok())
+}
+
+fn get_brightness(event: &Event) -> Option<u16> {
+    let brightness: u16 = get_attribute(event, "brightness")?;
+    let max_brightness: u16 = get_attribute(event, "max_brightness")?;
+    Some(brightness * 100 / max_brightness)
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> io::Result<()> {
-    let mut monitor: AsyncMonitorSocket = MonitorBuilder::new()?
-        .match_subsystem("backlight")?
-        .listen()?
-        .try_into()?;
-
-    let mut old_value: u16 = 0;
-    while let Some(event) = monitor.next().await {
-        let event = event?;
-        let Some(brightness): Option<u16> = event
-            .attribute_value("brightness")
-            .and_then(OsStr::to_str)
-            .and_then(|s| s.parse().ok())
-        else {
-            continue;
-        };
-        let Some(max_brightness): Option<u16> = event
-            .attribute_value("max_brightness")
-            .and_then(OsStr::to_str)
-            .and_then(|s| s.parse().ok())
-        else {
-            continue;
-        };
-        let target = brightness * 100 / max_brightness;
-        if target == old_value {
-            continue;
+    let mut brightness: u16 = 0;
+    AsyncMonitorSocket::try_from(
+        MonitorBuilder::new()?
+            .match_subsystem("backlight")?
+            .listen()?,
+    )?
+    .try_filter_map(|event| async move { Ok(get_brightness(&event)) })
+    .try_for_each(|new_brightness| async move {
+        if new_brightness != brightness {
+            brightness = new_brightness;
+            for mut display in Display::enumerate() {
+                display.handle.set_vcp_feature(0x10, brightness).unwrap();
+            }
         }
-        old_value = target;
-
-        for mut display in Display::enumerate() {
-            display.handle.set_vcp_feature(0x10, target).unwrap();
-        }
-    }
-
-    Ok(())
+        Ok(())
+    })
+    .await
 }
