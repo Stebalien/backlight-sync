5e7c0837506266f8c82d24a2eccb6b409b124d67
Steven Allen <steven@stebalien.com>
2024-07-19 11:16:58 -0700

    defer update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::ffi::OsStr;
 use std::io;
 use std::str::FromStr;
+use std::time::Duration;
 
 use ddc_hi::{Ddc, Display};
 use futures::stream::StreamExt;
+use tokio::time;
 use tokio_udev::{AsyncMonitorSocket, Device, Enumerator, MonitorBuilder};
+
+const UPDATE_DELAY: Duration = Duration::from_secs(1);
 
 fn get_attribute<T: FromStr>(dev: &Device, attr: &str) -> Option<T> {
     dev.attribute_value(attr)
@@ -28,6 +32,23 @@ fn get_initial_brightness() -> io::Result<Option<u16>> {
         .next())
 }
 
+async fn update_brightness(brightness: u16) {
+    time::sleep(UPDATE_DELAY).await;
+    'retry: for _ in 0..3 {
+        for mut display in Display::enumerate() {
+            if let Err(e) = display.handle.set_vcp_feature(0x10, brightness) {
+                log::warn!(
+                    "failed to set brightness for display {}: {}",
+                    display.info,
+                    e
+                );
+                continue 'retry;
+            };
+        }
+        break;
+    }
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> io::Result<()> {
     env_logger::init();
@@ -38,8 +59,24 @@ async fn main() -> io::Result<()> {
             .match_subsystem("drm")?
             .listen()?,
     )?;
-    while let Some(event) = monitor.next().await {
-        let event = event?;
+
+    let mut update_task = None;
+    loop {
+        let Some(event) = if let Some(task) = update_task.take() {
+            tokio::select! {
+                event = monitor.next() => event,
+                _ = task => {
+                    monitor.next().await
+                }
+            }
+        } else {
+            monitor.next().await
+        }
+        .transpose()?
+        else {
+            return Ok(());
+        };
+
         match event.device().subsystem().and_then(OsStr::to_str) {
             Some("drm") => {
                 // refresh
@@ -58,19 +95,6 @@ async fn main() -> io::Result<()> {
             }
             _ => continue,
         }
-        'retry: for _ in 0..3 {
-            for mut display in Display::enumerate() {
-                if let Err(e) = display.handle.set_vcp_feature(0x10, brightness) {
-                    log::warn!(
-                        "failed to set brightness for display {}: {}",
-                        display.info,
-                        e
-                    );
-                    continue 'retry;
-                };
-            }
-            break;
-        }
+        update_task = Some(update_brightness(brightness));
     }
-    Ok(())
 }
