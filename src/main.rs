edfdb66bbb47be88795c43aba22d78a11c46e1eb
Steven Allen <steven@stebalien.com>
2025-03-30 21:59:46 -0700

    make it more robust
    1. Check if displays support the feature.
    2. Better handle display disconnects.
    3. Wait while displays are changing before updating things to prevent
    crashing drivers (grr...).

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ use std::future::IntoFuture;
 use std::io;
 use std::str::FromStr;
 use std::sync::{Arc, Mutex};
+use std::time::Duration;
 
 use ddc_hi::{Ddc, Display};
 use futures::stream::StreamExt;
+use mccs_db::Access;
 use tokio::task::JoinError;
 use tokio_udev::{AsyncMonitorSocket, Device, Enumerator, MonitorBuilder};
+
+const MONITOR_CHANGE_DELAY: Duration = Duration::from_secs(5);
+const VCP_SET_BRIGHTNESS: u8 = 0x10;
 
 fn get_attribute<T: FromStr>(dev: &Device, attr: &str) -> Option<T> {
     dev.attribute_value(attr)
@@ -31,33 +36,57 @@ fn get_initial_brightness() -> io::Result<Option<u16>> {
         .next())
 }
 
-async fn update_brightness(displays: Vec<Arc<Mutex<Display>>>, brightness: u16) {
+async fn update_brightness(displays: &[Arc<Mutex<Display>>], brightness: u16) -> bool {
     let mut js = tokio::task::JoinSet::new();
-    for display in displays {
+    #[allow(clippy::unnecessary_to_owned)] // clippy is drunk here.
+    for display in displays.iter().cloned() {
         js.spawn_blocking(move || {
             let mut display = display.lock().unwrap();
             for _ in 0..3 {
-                if let Err(e) = display.handle.set_vcp_feature(0x10, brightness) {
-                    log::warn!(
+                match display.handle.set_vcp_feature(0x10, brightness) {
+                    Ok(_) => return true,
+                    Err(e) => log::warn!(
                         "failed to set brightness for display {}: {}",
                         display.info,
                         e
-                    );
-                } else {
-                    break;
+                    ),
                 }
             }
+            false
         });
     }
-    let _ = js.join_all().await;
+    js.join_all().await.into_iter().all(|b| b)
 }
 
 async fn enumerate() -> Result<Vec<Arc<Mutex<Display>>>, JoinError> {
     tokio::task::spawn_blocking(|| {
-        Display::enumerate()
-            .into_iter()
-            .map(|d| Arc::new(Mutex::new(d)))
-            .collect()
+        let displays = Display::enumerate();
+        let mut output = Vec::with_capacity(displays.len());
+        for mut display in displays {
+            if let Err(e) = display.update_capabilities() {
+                log::error!(
+                    "failed to update display capabilities for {}, skipping display: {}",
+                    display.info,
+                    e
+                );
+                continue;
+            }
+            match display.info.mccs_database.get(VCP_SET_BRIGHTNESS) {
+                Some(v) => match v.access {
+                    Access::WriteOnly | Access::ReadWrite => {}
+                    Access::ReadOnly => {
+                        log::debug!(
+                            "skipping display {}, cannot update brightness",
+                            display.info
+                        );
+                        continue;
+                    }
+                },
+                None => continue,
+            }
+            output.push(Arc::new(Mutex::new(display)));
+        }
+        output
     })
     .into_future()
     .await
@@ -76,13 +105,15 @@ async fn main() -> io::Result<()> {
     )?;
 
     let mut brightness: u16 = get_initial_brightness()?.unwrap_or(255);
-    update_brightness(displays.clone(), brightness).await;
+    update_brightness(&displays, brightness).await;
     while let Some(event) = monitor.next().await {
         let event = event?;
         match event.device().subsystem().and_then(OsStr::to_str) {
             Some("drm") => {
                 // refresh
                 log::info!("drm change, updating backlight");
+                // I wait a second here to avoid...
+                tokio::time::sleep(MONITOR_CHANGE_DELAY).await;
                 displays = enumerate().await?;
             }
             Some("backlight") => {
@@ -95,9 +126,12 @@ async fn main() -> io::Result<()> {
                 }
                 log::info!("changing backlight from {brightness} to {new_brightness}");
                 brightness = new_brightness;
-                update_brightness(displays.clone(), brightness).await;
             }
-            _ => {}
+            _ => continue,
+        }
+        if !update_brightness(&displays, brightness).await {
+            log::warn!("failed to set brightness for some displays, refreshing display list");
+            displays = enumerate().await?;
         }
     }
     Ok(())
